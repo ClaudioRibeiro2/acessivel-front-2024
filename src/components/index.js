88f5f7bf58437ae2cd485615d20367178ed03c43
ClaudioRibeiro2 <claudio_ribeiro2@estudante.sc.senai.br>
2024-11-30 14:11:01 -0300

    feat: Excluido componente inultil

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ export { default as RegisterUserForm } from "./RegisterUserForm";
 export { default as AppSideBar } from "./AppSideBar";
 export { default as AcessivelIcon} from "./AcessivelIcon";
 export { default as AddressUserForm } from "./AddressUserForm";
-export { default as Tabs } from "./Tabs";
 export { default as TabsRequirements } from "./TabsRequirements" 
 export { default as TabsPersonalData } from "./TabsPersonalData"
 export { default as CardNavigator } from "./CardNavigator"
